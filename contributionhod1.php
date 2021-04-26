19cdaf62b6b0fd216cd0bec56d534e65e13b5e1e
GunjanShekhat <47917792+GunjanShekhat@users.noreply.github.com>
2021-04-26 22:24:07 +0530

    Initial Commit

---
+++ contributionhod1.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+
+    <title>FORM B (Section 4)</title>
+<!--
+SOFTY PINKO
+https://templatemo.com/tm-535-softy-pinko
+-->
+
+    <!-- Additional CSS Files -->
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"> -->
+
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css"> -->
+
+    <link rel="stylesheet" href="assets/css/templatemo-softy-pinko.css">
+
+
+
+    <link rel="stylesheet" href="assets/css/style1.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    <body>
+        
+        
+    
+    
+    </div>
+<div>
+    <?php
+        include('headerhod.php');
+    ?> 
+    </div>
+    <div class="container bootstrap snippets bootdey">
+        <div class="row">
+            <div class="profile-nav col-md-3">
+                <div class="panel">
+                    <div class="user-heading round">
+                        <a href="#">
+                            <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
+                        </a>
+                        <h1>Camila Smith</h1>
+                        <p>deydey@theEmail.com</p>
+                    </div>
+
+                    
+                </div>
+            </div>
+            <div class="profile-info col-md-9">
+                <div class="panel">
+                    
+                        <textarea placeholder="Performance Appraisal" rows="2"
+                            class="form-control input-lg p-text-area"></textarea>
+                </div>
+                <div class="panel">
+                    <div class="bio-graph-heading">
+                    FORM B (Section 4)
+                    </div>
+                    <div class="panel-body bio-graph-info">
+                        
+                        <div class="row">
+                            <div class="panel-body bio-graph-info" style="background-color:white">
+                                <!--<h2 style="color:blanchedalmond;">FORM B</h2>-->
+                                <h3 style="color:black;"> Contribution to TRUST/SOCIETY/UNIVERSITY</h3>
+                                
+                                <form action="" method="POST">
+                                    <select id="Faculty" name="Faculty">
+                                        
+                                      <input type="submit">
+                                </form>
+                                <br>
+                                <form action="" method="POST">
+                                <table class="table ">
+                                
+                                    <thead>
+                                        <tr>
+                                            <th scope="col" style="text-align: left; width: 50px;">Section number </th>
+                                            <th scope="col" style="text-align:center;  width: 280px;">Nature of activity</th>
+                                            <th scope="col" style="text-align: center;  width: 70px; ">Maximum API score </th>
+                                            <th scope="col" style="text-align: center;">Self Rating</th>
+                                            <th scope="col" style="text-align: center;">HOD Rating</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row"  style="text-align: center;" >1</th>
+                                            <td  style="text-align: left;"><div allign="justify">
+                                              <div align="justify">Placement support (MoU's created form the Industries at National and International level) (2 Points/ activity, max. 05 points all together)</div>
+                                            </div></td>
+                                            <td style="text-align: center;">5</td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input type="number" placeholder="0-5" aria-label="0-5" min="0" max="5"
+                                                    style="width: 135px;">
+                                                </div>
+                                            </div></td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input name="number" type="number"
+                                                    style="width: 135px;" placeholder="0-5" aria-label="0-5" min="0" max="5">
+                                                </div>
+                                            </div></td>
+                                        <tr>
+                                            <th scope="row"  style="text-align: center;" >2</th>
+                                            <td  style="text-align: left;"><div allign="justify">
+                                              <div align="justify">Improvement in University organized Programs; e.g. Persona Fest etc…, (5 points/ activity, max. 05 points)</div>
+                                            </div></td>
+                                            <td style="text-align: center;">5</td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input type="number" placeholder="0-5" aria-label="0-5" min="0" max="5"
+                                                    style="width: 135px;">
+                                                </div>
+                                            </div></td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input type="number" placeholder="0-5" aria-label="0-5" min="0" max="5"
+                                                    style="width: 135px;">
+                                                </div>
+                                            </div></td>
+                                        <tr>
+                                            <th scope="row"  style="text-align: center;" >3</th>
+                                            <td  style="text-align: left;"><div allign="justify">
+                                              <div align="justify">Involvement in University Admission process, Exhibition Drive/ Education Expo, Branding work and School of Holistic Development activities (2 points/ activity/ semester, Max. 05 points all together)</div>
+                                            </div></td>
+                                            <td style="text-align: center;">5</td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input type="number" placeholder="0-5" aria-label="0-5" min="0" max="5"
+                                                    style="width: 135px;">
+                                                </div>
+                                            </div></td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input type="number" placeholder="0-5" aria-label="0-5" min="0" max="5"
+                                                    style="width: 135px;">
+                                                </div>
+                                            </div></td>
+                                         <tr>
+                                            <th scope="row"  style="text-align: center;" >4</th>
+                                            <td  style="text-align: left;"><div allign="justify">
+                                              <div align="justify">Value Addition Programmes (other than regular syllabus which is helpful to the students) (2 points/ activity/ semester, max. 05 points all together)</div>
+                                            </div></td>
+                                            <td style="text-align: center;">05</td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input type="number" placeholder="0-5" aria-label="0-5" min="0" max="5"
+                                                    style="width: 135px;">
+                                                </div>
+                                            </div></td>
+                                            <td><div allign="center">
+                                              <div align="center">
+                                                <input type="number" placeholder="0-5" aria-label="0-5" min="0" max="5"
+                                                    style="width: 135px;">
+                                                </div>
+                                            </div></td>
+                                            <tr>
+                                                <th scope="row"  style="text-align: center;" ></th>
+                                                <td  style="text-align: center;"><b>Total</b></td>
+                                                <td style="text-align: center;">20</td>
+                                                <td><div allign="center">
+                                                  <div align="center">
+                                                    <input type="number" placeholder="0-20" aria-label="0-20" min="0" max="20"
+                                                        style="width: 135px;">
+                                                    </div>
+                                                </div></td>
+                                                <td><div allign="center">
+                                                  <div align="center">
+                                                    <input type="number" placeholder="0-20" aria-label="0-20" min="0" max="20"
+                                                        style="width: 135px;">
+                                                    </div>
+                                                </div></td>
+                                            <tr>
+                                                <th scope="row"  style="text-align: center;" ></th>
+                                                <td  style="text-align: center;"><b>Minimum API score required</b></td>
+                                                <td style="text-align: center;">10</td>
+                                                <td><div allign="center">
+                                                  <div align="center">
+                                                    <input type="number" placeholder="0-10" aria-label="0-10" min="0" max="10"
+                                                        style="width: 135px;">
+                                                    </div>
+                                                </div></td>
+                                                <td><div allign="center">
+                                                  <div align="center">
+                                                    <input type="number" placeholder="0-10" aria-label="0-10" min="0" max="10"
+                                                        style="width: 135px;">
+                                                    </div>
+                                                </div></td>
+                                    </tbody>
+                                </table>
+                                <p><span>Director/Principal remarks:  </span>: <input type="text"
+                                    style="width:400px;"></p>
+                                <br>
+                              <br>
+                                
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                                <br>
+                        </div>
+                    </div>
+                </div>
+            </div>      
+                                
+        </div>
+
+    
+    </div>
+    </div>
+    </div>
+    </div>
+    
+        <?php
+        include ('footer.php');
+        ?>
+    </div>
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+
+    </script>
+</br>
+</br>
+
+    
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+<!--  -->
+
+
+    
+  </body>
+</html>
---
+++ formB1director.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+
+    <title>FORM A (Section 1)</title>
+<!--
+SOFTY PINKO
+https://templatemo.com/tm-535-softy-pinko
+-->
+
+    <!-- Additional CSS Files -->
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"> -->
+
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css"> -->
+
+    <link rel="stylesheet" href="assets/css/templatemo-softy-pinko.css">
+
+
+
+    <link rel="stylesheet" href="assets/css/style1.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    <body>
+        
+        
+    
+   
+    <?php
+        include('headerdirector.php');
+    ?> 
+    </div>
+
+
+    <div class="container bootstrap snippets bootdey">
+<div class="row">
+  <div class="profile-nav col-md-3">
+      <div class="panel">
+          <div class="user-heading round">
+              <a href="#">
+                  <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
+              </a>
+              <h1>Camila Smith</h1>
+              <p>deydey@theEmail.com</p>
+          </div>
+
+          
+      </div>
+  </div>
+  <div class="profile-info col-md-9">
+      <div class="panel">
+          <form>
+              <textarea placeholder="Performance Appraisal" rows="2" class="form-control input-lg p-text-area"></textarea>
+          </form>
+      </div>
+      <div class="panel">
+          <div class="bio-graph-heading">
+          FORM A (Section 1)
+          </div>
+          <div class="panel-body bio-graph-info">
+              <h1>PERSONAL INFORMATION</h1>
+              <div class="row">
+                  <div class="bio-row">
+                      <p><span>First Name </span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Last Name </span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Father's Name</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>No. of Children</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Date of birth</span><input type="date" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Gender</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Departments</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Marital Status:</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Nationality</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Religion</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Caste</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Sub-Category</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>School name and department name</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Permanent Address </span><textarea class="form-control" id="form6Example7" rows="4"></textarea></p>
+                  </div>
+                  
+                  <div class="bio-row">
+                      <p><span>Address of correspondance</span><textarea class="form-control" id="form6Example7" rows="4"></textarea></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Current designation and grade pay</span> <input class="input" value="" /></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Email </span><input type="email" class="form-control" placeholder="" aria-label=""></p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Mobile </span><input type="number" class="form-control" placeholder="" aria-label=""></p>
+                  </div>
+                  <div class="w-25 p-3" style="width:800px; margin:0 auto;"  >
+                      <p><span>Whether acquired and Degrees/Fresh academic Qualifications this year:</span></span> <input class="input" value="" /></p>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div> 
+                  </div>
+              </div>
+          </div>
+      </div>
+      </div>
+      </div>
+      <div>
+        <?php
+        include ('footer.php');
+        ?>
+    </div>
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+
+    </script>
+</br>
+</br>
+
+    
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+<!--  -->
+
+
+    
+  </body>
+</html>
