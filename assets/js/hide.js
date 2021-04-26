d5aa05957c7ac6f4b573185ffb84911df3fa522f
GunjanShekhat <47917792+GunjanShekhat@users.noreply.github.com>
2021-04-26 22:30:54 +0530

    Initial Commit

---
+++ assets/js/hide.js
@@ -0,0 +1,9 @@
+$('#frsradio').click(function() {
+    $('.txbx').attr('hidden',false);
+     $('.cars').attr('hidden',false);
+});           
+$('#expradio').click(function() {
+    $('.txbx').attr('hidden',true);
+    $('.cars').attr('hidden',true);
+});
+});
